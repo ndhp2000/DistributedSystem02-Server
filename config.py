3c05ac0a04f551d2fc4aec46cacc05e39567b79b
Nguyen Dinh Hoang Phuc <ndhp2000@gmail.com>
2021-12-26 21:44:10 +0700

    add Requirements

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ LOGGER = [("game-debug", "./logs"), ("game-view", "./logs"), ("game-controller",
 PROCESSED_EVENTS_PER_LOOPS = 1000
 FRAME_RATE = 45
 FRAME_RATE_MS = int(1000 / FRAME_RATE)
-MAX_PING = 8  # frames
+MAX_PING = 4  # frames
 
 MAP_WIDTH = 32
 MAP_HEIGHT = 16
